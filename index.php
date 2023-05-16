88b0336b54fa2dfb9e32ce9adf2e04c660f99253
LMI SERVICE <lm-ismael@hotmail.com>
2023-05-16 16:47:56 +0000

    Commit de la page login

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="img/logo.ico" type="image/x-icon">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Chat App - Login</title>
+</head>
+
+<body class="d-flex
+             justify-content-center
+             align-items-center
+             vh-100">
+    <div class="w-400 p-5 shadow rounded">
+        <form class="d-flex
+                     justify-content-center
+                     align-items-center
+                     flex-column">
+                     <img src="img/logo.png" 
+                          class="w-25">
+            <h3 class="display-4 fs-1 text-center">
+                LOGIN </h3>
+
+            <div class="form-group">
+                <label class="form-label">
+                       User name</label>
+                <input type="email" 
+                       class="form-control">
+            </div>
+            <div class="form-group">
+                <label class="form-label">
+                       Password</label>
+                <input type="email" 
+                       class="form-control">
+            </div>
+            
+            <button type="submit" 
+                    class="btn btn-primary">
+                    LOGIN</button>
+            <a href="signuo.php">Sign up</a>
+        </form>
+    </div>
+</body>
+
+</html>
